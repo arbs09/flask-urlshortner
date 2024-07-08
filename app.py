f4419a212c0a50dc312df749cfff6674a12963a1
arbs09 <info@arbs09.de>
2024-07-08 08:20:37 +0200

    mapping out and creating other

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, send_from_directory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('')
+@app.route('/')
 def index():
     return('Hello World')
+
+@app.route('/dashboard')
+def dashboard():
+    return('The Dashboard will be here!')
+
+
+
+# other
+
+@app.route('/robots.txt')
+def robots_txt():
+    return send_from_directory(app.static_folder, 'robots.txt')
+
+@app.route('/.well-known/security.txt')
+def security_txt():
+    return send_from_directory(app.static_folder, 'security.txt')
